4145ada4e3dbea8e2fb971bf3338861f188924cb
javyepes <javyepes@gmail.com>
2016-03-28 10:13:21 -0400

    fixed hardcoded variable

--- scripts/report-available-patches-centos.sh
+++ scripts/report-available-patches-centos.sh
@@ -1,7 +1,7 @@
 # count number of packages thats can be upgraded
 DATE=`date +%Y-%m-%d`
 email_rec=""
-server_list=`awk -F" " '{print $1}' server-centos.list` # I am using a comma separated list
+server_list=`awk -F" " '{print $1}' server-centos.list` # I am using a comma separated list "server-centos.list"
 raw_report="/tmp/$DATE-raw-report-data.txt" ; touch $raw_report && echo "" > $raw_report # create and nulls out the file
 quick_report="/tmp/$DATE-quick-report.txt" ; touch $quick_report && echo "" > $quick_report # create and nulls out the file
 full_report="/tmp/$DATE-full-report.txt" ; touch $full_report && echo "" > $full_report # create and nulls out the file
@@ -20,7 +20,7 @@ elif [[ $mode = "-v" ]];then
   cat $full_report|mail -s "CentOS: $DATE full patch report"  
   echo "complete"
 elif [[ $mode = "-s" ]];then
-  for server in `awk -F" " '{print $1}' ../lists/server-cloud-centos.list`;do
+  for server in `awk -F" " '{print $1}' $server_list`;do
     ssh -t $server "echo $DATE && hostname && sudo yum list updates|grep -A 500 'Updated Packages' " > $raw_report # dumps stdout to $raw_report file
     echo "`head -2 $raw_report|tail -1 `" |tee -a $quick_report && echo "There are `wc -l $raw_report|awk '{print $1}' ` package(s) to update" |tee -a $quick_report
     sed -i '/^$/d' $quick_report # removes blank lines
